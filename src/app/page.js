9fade6e929269b57b98748e227f4f6a29059aa4b
Uhakika <uhakika.tech@gmail.com>
2024-01-06 14:34:46 +0000

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import ResponsiveAppBar from "@/components/landingpage/ResponsiveAppBar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <ResponsiveAppBar />
+    </main>
+  )
+}
