9a27e5989919de395d8f8668f5a4c6ae3d0a01d7
Mutugi Riungu <colrium@gmail.com>
2024-02-14 07:36:20 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
+	transpilePackages: ['@mui/x-charts'],
 	async rewrites() {
 		return [
 			{
 				source: '/api/:slug*',
-				destination: 'http://localhost:5000/v1/:slug*'
+				destination: 'http://146.190.50.223:5000//v1/:slug*'
 			}
 		];
 	}
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
